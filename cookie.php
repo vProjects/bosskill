d9192e2c2b438da5c9f6f8c4382a83e4e6fa6ebd
vasu naman <github@vyrazu.com>
2013-10-16 20:19:48 +0530

    Initial Commit
    Initial Commit done by Vasu Naman

---
+++ cookie.php
@@ -0,0 +1,11 @@
+<?php
+ 
+ print_r($GLOBALS);
+ setcookie("name","naman");
+ echo $_SERVER['HTTP_REFERER']
+
+
+?>
+
+<a href="cookie.php">click me</a>
+<script src="//embed.flowplayer.org/5.4.3/embed.min.js"><div class="flowplayer" data-origin="http://localhost/localprojects/upload/flowplayer/" style="width: 624px; height: 260px;"><video><source type="video/webm" src="http://stream.flowplayer.org/bauhaus/624x260.webm"><source type="video/mp4" src="http://www.w3schools.com/tags/movie.mp4"><source type="video/ogv" src="http://www.w3schools.com/tags/movie.ogg"></video></div></script>
